abe063f9593129d41e9b0d8fe0b4410a1771c127
Michael Cohen <macball2000@yahoo.com>
2023-11-25 17:11:00 -0500

    Dashboard: SMS Init

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,16 +1,19 @@
 import { CreatePost } from "~/app/_components/create-post";
+import SMSDashboard from "~/app/_components/SMSDashboard";
 import { getServerAuthSession } from "~/server/auth";
 import { redirect } from "next/navigation";
+import { api } from "~/trpc/server";
 
-export default async function Dashboard() {
+export default async function DashboardPage() {
     const session = await getServerAuthSession();
     if(session?.user.role !== "admin"){
-        redirect('/admin/login')
+        return redirect('/admin/login')
     }
   return (
-    <div>
+    <div className='h-screen flex flex-col justify-start items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white'>
         <h1>Dashboard</h1>
         <p>Welcome back, {session?.user.firstName}</p>
+        <SMSDashboard/>
     </div>
   )
 }
