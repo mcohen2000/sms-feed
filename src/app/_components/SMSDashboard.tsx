7358b507dd8cb9e7fcb9e711c9ef50ff0b036bac
Michael Cohen <macball2000@yahoo.com>
2023-11-26 15:16:55 -0500

    Dashboard: Move Buttons to Client Component

--- src/app/_components/SMSDashboard.tsx
+++ src/app/_components/SMSDashboard.tsx
@@ -1,12 +1,18 @@
 import { api } from "~/trpc/server";
+import Link from "next/link";
+import PostButtons from "./PostButtons";
 
 export default async function Dashboard() {
   const allPosts = await api.post.getAll.query();
   return (
     <div className="w-[80%] rounded-md bg-white text-black">
       <div className="flex flex-wrap items-center justify-between gap-2 border-b p-2">
-        <button className="rounded-md border px-2 py-1 bg-blue-500 text-white">New</button>
-        <div className="flex items-center gap-1 flex-wrap">
+        <Link href={"/admin/dashboard/new"}>
+          <button className="rounded-md border bg-blue-500 px-2 py-1 text-white">
+            New
+          </button>
+        </Link>
+        <div className="flex flex-wrap items-center gap-1">
           Filter:
           <input
             type="text"
@@ -26,18 +32,10 @@ export default async function Dashboard() {
                 key={post.id}
                 className="flex w-full items-center justify-between border-b px-2 py-4"
               >
-                <span className="truncate" title={post.name}>{post.name}</span>
-                <span className="flex gap-1 pl-1">
-                  <button className="rounded-md border bg-green-500 px-2 py-1">
-                    Send
-                  </button>
-                  <button className="rounded-md border bg-yellow-500 px-2 py-1">
-                    Edit
-                  </button>
-                  <button className="rounded-md border bg-red-500 px-2 py-1">
-                    Delete
-                  </button>
+                <span className="truncate" title={post.name}>
+                  {post.name}
                 </span>
+                <PostButtons id={post.id}/>
               </li>
             );
           })
