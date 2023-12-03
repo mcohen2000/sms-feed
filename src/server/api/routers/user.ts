398e2aac69dc0b0592a3d0d11077b282fd4de2b2
Michael Cohen <macball2000@yahoo.com>
2023-12-03 17:15:20 -0500

    Analytics: Total Sub Count + Monthly Count Routes

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -4,30 +4,33 @@ import { hash } from "bcrypt";
 
 import {
   createTRPCRouter,
+  protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
   create: publicProcedure
-    .input(z.object({ 
-      first_name: z.string().min(1), 
-      last_name: z.string().min(1), 
-      email: z.string().min(1), 
-      password: z.string().min(1), 
-    }))
+    .input(
+      z.object({
+        first_name: z.string().min(1),
+        last_name: z.string().min(1),
+        email: z.string().min(1),
+        password: z.string().min(1),
+      }),
+    )
     .mutation(async ({ ctx, input }) => {
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
       const exists = await ctx.db.user.findFirst({
-        where: { email: input.email }
+        where: { email: input.email },
       });
-      if(exists){
+      if (exists) {
         throw new TRPCError({
           code: "CONFLICT",
-          message: "This email is already registered to a user."
-        })
+          message: "This email is already registered to a user.",
+        });
       }
-      const hashedPassword = await hash(input.password, 10)
+      const hashedPassword = await hash(input.password, 10);
 
       return ctx.db.user.create({
         data: {
@@ -39,25 +42,43 @@ export const userRouter = createTRPCRouter({
       });
     }),
   subscribe: publicProcedure
-    .input(z.object({ 
-      phone: z.string().min(10), 
-    }))
+    .input(
+      z.object({
+        phone: z.string().min(10),
+      }),
+    )
     .mutation(async ({ ctx, input }) => {
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
       const exists = await ctx.db.subscriber.findFirst({
-        where: { phone: input.phone }
+        where: { phone: input.phone },
       });
-      if(exists){
+      if (exists) {
         throw new TRPCError({
           code: "CONFLICT",
-          message: "This number is already registered to a user."
-        })
-      };
+          message: "This number is already registered to a user.",
+        });
+      }
       return ctx.db.subscriber.create({
         data: {
           phone: input.phone,
         },
       });
     }),
+  subscriberCount: protectedProcedure.query(async ({ ctx }) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return ctx.db.subscriber.count();
+  }),
+  subscriberMonthlyCount: protectedProcedure.query(async ({ ctx }) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return ctx.db.$queryRaw`SELECT 
+      DATE_TRUNC('month', "createdAt") AS month,
+      COUNT(*) AS signUpsCount
+    FROM
+      public."Subscriber"
+    GROUP BY
+      month
+    ORDER BY
+      month ASC;`;
+  }),
 });
