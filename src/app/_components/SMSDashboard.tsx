5193a1bff1d5fac85c06d8108c9e385dfc4fd004
Michael Cohen <macball2000@yahoo.com>
2023-12-04 17:04:20 -0500

    SMSDashboard: Styles + No Search Results Message

--- src/app/_components/SMSDashboard.tsx
+++ src/app/_components/SMSDashboard.tsx
@@ -17,7 +17,7 @@ export default async function Dashboard(searchParams: {
   });
   return (
     <div className="w-full rounded-md bg-white text-black">
-      <div className="flex flex-wrap items-center justify-between gap-2 border-b p-2">
+      <div className="flex flex-wrap items-center justify-between gap-2 border-b p-4">
         <h2 className="text-lg font-bold">Post Manager</h2>
         <PostFilters />
       </div>
@@ -31,21 +31,22 @@ export default async function Dashboard(searchParams: {
           <li>Loading Posts...</li>
         )}
       </ul>
-      <div className="flex items-center justify-center gap-6 py-2">
+      <div className="flex items-center justify-center gap-6 py-4">
         {/* pagination nav */}
         {searchParams.page && parseInt(searchParams.page) > 1 ? (
           <Link
+            className="w-[36px] text-center"
             href={`/admin/dashboard?view=posts${searchParams.search ? `&search=${searchParams.search}`:""}${searchParams.sent ? `&sent=${searchParams.sent}`:""}&page=${
               parseInt(searchParams.page) - 1
             }`}
           >
             Prev
           </Link>
-        ) : (
-          ""
-        )}
+        ) : 
+          postsCount > 5 ? <span className="w-[36px] text-center opacity-0">Prev</span> : null
+        }
         {postsCount > 5 ? searchParams.page ? (
-          <span>
+          <span className="w-[90px] text-center">
             {(parseInt(searchParams.page)-1 )* 5 + 1}
             -
             {(parseInt(searchParams.page)) * 5 > postsCount
@@ -56,16 +57,17 @@ export default async function Dashboard(searchParams: {
         ) : (
           <span>
             {"1 - "}
-            {5 > postsCount
+            {postsCount < 5
               ? postsCount
               : 5}{" "}
             of {postsCount}
           </span>
-        ): <span>{postsCount} of {postsCount}</span>}
+        ): postsCount !== 0 ? <span>{postsCount} of {postsCount}</span> : <span>No results for "{searchParams.search}"</span>}
 
         {searchParams.page ? (
           5 * parseInt(searchParams.page) < postsCount ? (
             <Link
+              className="w-[36px] text-center"
               href={`/admin/dashboard?view=posts${searchParams.search ? `&search=${searchParams.search}`:""}${searchParams.sent ? `&sent=${searchParams.sent}`:""}&page=${
                 searchParams.page ? parseInt(searchParams.page) + 1 : 2
               }`}
@@ -73,7 +75,7 @@ export default async function Dashboard(searchParams: {
               Next
             </Link>
           ) : (
-            ""
+            <span className="w-[36px] text-center opacity-0">Next</span>
           )
         ) : postsCount > 5 ? (
           <Link
