1df8128fed8bb99a7b356f72ccc9791dd3ec72e8
Michael Cohen <macball2000@yahoo.com>
2023-12-15 20:17:19 -0500

    Post: Handle Inbound Webhooks

--- src/app/api/twilio/inbound/route.tsx
+++ src/app/api/twilio/inbound/route.tsx
@@ -1,18 +1,41 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
+import { api } from "~/trpc/server";
 
+export async function POST(req: NextRequest) {
+  const data = await req.formData();
+  const toCountry = data.get("ToCountry")?.toString() || "";
+  const toState = data.get("ToState")?.toString() || "";
+  const numMedia = data.get("NumMedia")?.toString() || "";
+  const toCity = data.get("ToCity")?.toString() || "";
+  const fromZip = data.get("FromZip")?.toString() || "";
+  const smsSid = data.get("MessageSid")?.toString() || "";
+  const optOutType = data.get("OptOutType")?.toString() || "";
+  const fromState = data.get("FromState")?.toString() || "";
+  const status = data.get("SmsStatus")?.toString() || "";
+  const fromCity = data.get("FromCity")?.toString() || "";
+  const body = data.get("Body")?.toString() || "";
+  const from = data.get("From")?.toString() || "";
+  const to = data.get("To")?.toString() || "";
+  const toZip = data.get("ToZip")?.toString() || "";
+  const numSegments = data.get("NumSegments")?.toString() || "";
 
-export async function POST(req: NextRequest){
-    console.log("inbound message!!!");
-    const data = await req.formData();
-    const from = data.get("From");
-    const messageSid = data.get("MessageSid");
-    const optOutType = data.get("OptOutType");
-    const body = data.get("body");
-    // toggle user if has OptOutType
-        // if (optOutType === "START")
-        // if (optOutType === "START")
-
-    // create Inbox Item?
-    console.log(data)
-    return NextResponse.json("Success");
+  // create InboundWebHook & handle Subscriber OptOut
+  const newInboundWebhook = api.post.handleInboundWebhook.mutate({
+    toCountry: toCountry,
+    toState: toState,
+    numMedia: parseInt(numMedia),
+    toCity: toCity,
+    fromZip: fromZip,
+    smsSid: smsSid,
+    optOutType: optOutType,
+    fromState: fromState,
+    status: status,
+    fromCity: fromCity,
+    body: body,
+    from: from,
+    to: to,
+    toZip: toZip,
+    numSegments: parseInt(numSegments),
+  });
+  return NextResponse.json("Success");
 }
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -29,12 +29,15 @@ export const postRouter = createTRPCRouter({
         },
       });
     }),
-    count: protectedProcedure.input(z.object({
-      search: z.string(),
-      sent: z.string(),
-    })).query(async ({ctx, input}) => {
-      if(input.sent === "false"){
-
+  count: protectedProcedure
+    .input(
+      z.object({
+        search: z.string(),
+        sent: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      if (input.sent === "false") {
         return ctx.db.post.count({
           where: {
             text: {
@@ -44,11 +47,10 @@ export const postRouter = createTRPCRouter({
             OutboundWebhook: {
               none: {},
             },
-          }
-        })
+          },
+        });
       }
-      if(input.sent === "true"){
-
+      if (input.sent === "true") {
         return ctx.db.post.count({
           where: {
             text: {
@@ -58,8 +60,8 @@ export const postRouter = createTRPCRouter({
             OutboundWebhook: {
               some: {},
             },
-          }
-        })
+          },
+        });
       }
       return ctx.db.post.count({
         where: {
@@ -67,9 +69,9 @@ export const postRouter = createTRPCRouter({
             contains: input.search || "",
             mode: "insensitive",
           },
-        }
-      })
-  }),
+        },
+      });
+    }),
   getAll: protectedProcedure
     .input(
       z.object({
@@ -82,7 +84,7 @@ export const postRouter = createTRPCRouter({
       await new Promise((resolve) => setTimeout(resolve, 1000));
       if (input.sent === "false") {
         return ctx.db.post.findMany({
-          skip: 5*(parseInt(input.page)-1) || 0,
+          skip: 5 * (parseInt(input.page) - 1) || 0,
           take: 5,
           orderBy: { createdAt: "desc" },
           include: {
@@ -101,7 +103,7 @@ export const postRouter = createTRPCRouter({
       }
       if (input.sent === "true") {
         return ctx.db.post.findMany({
-          skip: 5*(parseInt(input.page)-1) || 0,
+          skip: 5 * (parseInt(input.page) - 1) || 0,
           take: 5,
           orderBy: { createdAt: "desc" },
           include: {
@@ -119,7 +121,7 @@ export const postRouter = createTRPCRouter({
         });
       }
       return ctx.db.post.findMany({
-        skip: (5*(parseInt(input.page)-1)) || 0,
+        skip: 5 * (parseInt(input.page) - 1) || 0,
         take: 5,
         orderBy: { createdAt: "desc" },
         where: {
@@ -157,20 +159,24 @@ export const postRouter = createTRPCRouter({
     }),
   send: protectedProcedure
     .input(z.object({ id: z.string().min(1), schedule: z.string() }))
-    .mutation(async ({ctx, input}) => {
+    .mutation(async ({ ctx, input }) => {
       const post = await ctx.db.post.findFirst({
         where: { id: input.id },
-        include: { OutboundWebhook: true }
+        include: { OutboundWebhook: true },
       });
-      if (post && post.text){
-        if (input.schedule){
-          if(Date.parse(input.schedule) - Date.now() < 15*60*1000){
+      if (post && post.text) {
+        if (input.schedule) {
+          if (Date.parse(input.schedule) - Date.now() < 15 * 60 * 1000) {
             throw new TRPCError({
               code: "CONFLICT",
-              message: "Scheduled time is less than 15 minutes from current time.",
+              message:
+                "Scheduled time is less than 15 minutes from current time.",
             });
           }
-          if(Date.parse(input.schedule) - Date.now() > 7*24*60*60*1000){
+          if (
+            Date.parse(input.schedule) - Date.now() >
+            7 * 24 * 60 * 60 * 1000
+          ) {
             throw new TRPCError({
               code: "CONFLICT",
               message: "Scheduled date is more than 7 days from current date.",
@@ -178,25 +184,36 @@ export const postRouter = createTRPCRouter({
           }
         }
 
-        const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-        const subscribers = await ctx.db.subscriber.findMany({ include: { OutboundWebhook: true }});
-        subscribers.forEach(person => {
-          twilioClient.messages.create(input.schedule ? {
-            body: `${post.text}`,
-            to: person.phone,
-            sendAt: new Date(input.schedule),
-            scheduleType: "fixed",
-            messagingServiceSid: process.env.TWILIO_SERVICE_SID,
-          }: {
-            body: `${post.text}`,
-            to: person.phone,
-            messagingServiceSid: process.env.TWILIO_SERVICE_SID,
-          }).then( async (message) => {
-            // if(message.status === "accepted" || message.status === "scheduled"){
-              const outboundWebhook = await ctx.db.outboundWebhook.create({ 
+        const twilioClient = twilio(
+          process.env.TWILIO_ACCOUNT_SID,
+          process.env.TWILIO_AUTH_TOKEN,
+        );
+        const subscribers = await ctx.db.subscriber.findMany({
+          include: { OutboundWebhook: true },
+        });
+        subscribers.forEach((person) => {
+          twilioClient.messages
+            .create(
+              input.schedule
+                ? {
+                    body: `${post.text}`,
+                    to: person.phone,
+                    sendAt: new Date(input.schedule),
+                    scheduleType: "fixed",
+                    messagingServiceSid: process.env.TWILIO_SERVICE_SID,
+                  }
+                : {
+                    body: `${post.text}`,
+                    to: person.phone,
+                    messagingServiceSid: process.env.TWILIO_SERVICE_SID,
+                  },
+            )
+            .then(async (message) => {
+              // if(message.status === "accepted" || message.status === "scheduled"){
+              const outboundWebhook = await ctx.db.outboundWebhook.create({
                 include: {
                   post: true,
-                  subscriber: true
+                  subscriber: true,
                 },
                 data: {
                   apiVersion: message.apiVersion,
@@ -204,47 +221,116 @@ export const postRouter = createTRPCRouter({
                   smsStatus: message.status,
                   to: message.to,
                   from: message.from,
-                  subscriber: {connect: {id: person.id}},
-                  post: {connect: {id: post.id}},
-                }
-              })
-              await ctx.db.subscriber.update({ 
-                where: {id: person.id},
+                  subscriber: { connect: { id: person.id } },
+                  post: { connect: { id: post.id } },
+                },
+              });
+              await ctx.db.subscriber.update({
+                where: { id: person.id },
                 include: { OutboundWebhook: true },
                 data: {
-                  OutboundWebhook: {connect: {id: outboundWebhook.id}}
-                }
-              })
-              await ctx.db.post.update({ 
-                where: {id: post.id},
+                  OutboundWebhook: { connect: { id: outboundWebhook.id } },
+                },
+              });
+              await ctx.db.post.update({
+                where: { id: post.id },
                 include: { OutboundWebhook: true },
                 data: {
-                  OutboundWebhook: {connect: {id: outboundWebhook.id}}
-                }
-              })
-            // }
-          })
-          
-        })
-        return subscribers.length
+                  OutboundWebhook: { connect: { id: outboundWebhook.id } },
+                },
+              });
+              // }
+            });
+        });
+        return subscribers.length;
       }
-
     }),
   updateOutboundWebhook: publicProcedure
-  .input(z.object({ smsSid: z.string().min(1), status: z.string(), from: z.string().nullable(), doneDate: z.string().nullable() }))
-  .mutation(async ({ ctx, input }) => {
-    const exists = await ctx.db.outboundWebhook.findFirst({
-      where: { smsSid: input.smsSid }
-    });
-    if (exists && exists.smsStatus !== "delivered") {
-       return ctx.db.outboundWebhook.update({
-        where: {id: exists.id},
-        data: {
-          smsStatus: input.status,
-          from: input.from,
-          rawDlrDoneDate: exists.rawDlrDoneDate ? null : input.doneDate,
+    .input(
+      z.object({
+        smsSid: z.string().min(1),
+        status: z.string(),
+        from: z.string().nullable(),
+        doneDate: z.string().nullable(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const exists = await ctx.db.outboundWebhook.findFirst({
+        where: { smsSid: input.smsSid },
+      });
+      if (exists && exists.smsStatus !== "delivered") {
+        return ctx.db.outboundWebhook.update({
+          where: { id: exists.id },
+          data: {
+            smsStatus: input.status,
+            from: input.from,
+            rawDlrDoneDate: exists.rawDlrDoneDate ? null : input.doneDate,
+          },
+        });
+      }
+    }),
+  handleInboundWebhook: publicProcedure
+    .input(
+      z.object({
+        toCountry: z.string(),
+        toState: z.string(),
+        numMedia: z.number(),
+        numSegments: z.number(),
+        toCity: z.string(),
+        fromZip: z.string(),
+        smsSid: z.string().min(1),
+        optOutType: z.string(),
+        fromState: z.string(),
+        status: z.string(),
+        fromCity: z.string(),
+        body: z.string(),
+        to: z.string(),
+        toZip: z.string(),
+        from: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const subscriber = await ctx.db.subscriber.findFirst({
+        where: { phone: input.from },
+      });
+      console.log("SUBSCRIBER!!!!", subscriber)
+      if (subscriber) {
+        console.log("DB OPTOUT", subscriber.phone, subscriber.optedOut);
+        console.log("SMS OPTOUT", input.optOutType)
+        if (input.optOutType === "START" && subscriber.optedOut) {
+          console.log("START OPT MSG")
+          await ctx.db.subscriber.update({
+            where: { phone: subscriber.phone },
+            data: { optedOut: false },
+          });
         }
-      })
-    }
-  }),
+        if (input.optOutType === "STOP" && !subscriber.optedOut) {
+          console.log("STOP OPT MSG")
+          await ctx.db.subscriber.update({
+            where: { phone: subscriber.phone },
+            data: { optedOut: true },
+          });
+        }
+        return ctx.db.inboundWebhook.create({
+          data: {
+            from: input.from,
+            toCountry: input.toCountry,
+            toState: input.toState,
+            numMedia: input.numMedia,
+            toCity: input.toCity,
+            fromZip: input.fromZip,
+            smsSid: input.smsSid,
+            optOutType: input.optOutType,
+            fromState: input.fromState,
+            smsStatus: input.status,
+            fromCity: input.fromCity,
+            body: input.body,
+            to: input.to,
+            toZip: input.toZip,
+            numSegments: input.numSegments,
+            Subscriber: { connect: { id: subscriber.id } },
+          },
+        });
+      }
+    }),
 });
